d906bd54526e7957b59e61075762c687b0d2410c
feras <ferasish6@gmail.com>
2020-01-24 19:09:33 +0200

    First Push

---
+++ stockx/pages/main.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>CoolKicks</title>
+
+<link href="../css/main.css " rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1 , user-scaleable=no">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+
+
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+
+
+<body>
+
+
+
+  <nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+      <i class="fas fa-bars"></i>
+    </label>
+    <div  class="logo">
+<img href="main.php" src="../images/logo.png">
+</div>
+    <ul>
+      <li><a class="active" href="#">Home</a></li>
+      <li><a href="#">login</a></li>
+      <li><a href="#">signup</a></li>
+    </ul>
+  </nav>
+
+
+</body>
+
+
+</html>
---
+++ stockx/pages/shoes.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>CoolKicks</title>
+<link href="../css/navbar.css " rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1 , user-scaleable=no">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+
+
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+
+
+<body>
+
+
+
+  <nav>
+        <input type="checkbox" id="check">
+          <label for="check" class="checkbtn">
+                <i class="fas fa-bars"></i>
+         </label>
+    <div  class="logo">
+     <img href="main.php" src="../images/logo.png">
+   </div>
+    <ul>
+      <li><a class="active" href="shoes.php">shoes</a></li>
+      <li><a href="main.php">logout</a></li>
+    </ul>
+
+  </nav>
+
+
+
+
+
+
+
+
+
+
+
+</body>
